b1b91d24f3c284371205f218bd4f34497abba43a
quanvt.synx <quanvt.synx@gmail.com>
2024-10-07 21:35:29 +0700

    Initial Commit

---
+++ ScrumdingerTests/ScrumdingerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerTests.swift
+//  ScrumdingerTests
+//
+//  Created by Quan Tran on 7/10/24.
+//
+
+import Testing
+@testable import Scrumdinger
+
+struct ScrumdingerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
